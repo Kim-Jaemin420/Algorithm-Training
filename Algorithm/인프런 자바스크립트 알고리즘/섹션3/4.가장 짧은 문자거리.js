30f2fec8da2f136062f670ca95b083ec35f5aa3e
Kim-Jaemin420 <jany240@naver.com>
2021-05-01 23:58:12 +0900

    가장 짧은 문자거리

--- Algorithm/인프런 자바스크립트 알고리즘/섹션3/4.가장 짧은 문자거리.js
+++ Algorithm/인프런 자바스크립트 알고리즘/섹션3/4.가장 짧은 문자거리.js
@@ -1,3 +1,29 @@
+// 내 풀이
+function solution(s, t) {
+  const distance = [];
+  const tIndex = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === t) {
+      tIndex.push(i);
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    let min = Number.MAX_SAFE_INTEGER;
+    for (let j = 0; j < tIndex.length; j++) {
+      if (Math.abs(i - tIndex[j]) < min) min = Math.abs(i - tIndex[j]);
+    }
+    distance.push(min);
+  }
+
+  return distance;
+}
+
+const str = 'teachermode';
+console.log(solution(str, 'e'));
+
+// 강사님 답
 function solution(s, t) {
   let answer = [];
   let p = 1000;
@@ -21,5 +47,5 @@ function solution(s, t) {
   return answer;
 }
 
-const str = 'teachermode';
+let str = 'teachermode';
 console.log(solution(str, 'e'));
