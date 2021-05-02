ca67a5b9d6b7e41f420b60c33e2bfa5315b11017
Kim-Jaemin420 <jany240@naver.com>
2021-05-02 19:30:10 +0900

    자릿수의 합

---
+++ Algorithm/인프런 자바스크립트 알고리즘/섹션4/1.자리수의 합.js
@@ -0,0 +1,50 @@
+// 내 답
+function solution(n, arr) {
+  let answer;
+  let max = Number.MIN_SAFE_INTEGER;
+  let maxNum = 0;
+
+  for (let i = 0; i < n; i++) {
+    const toStr = arr[i] + '';
+    let sum = 0;
+    for (let j = 0; j < toStr.length; j++) {
+      sum += +toStr[j];
+    }
+    if (sum > max) {
+      max = sum;
+      maxNum = arr[i];
+    } else if (sum === max) {
+      maxNum = arr[i] > maxNum ? arr[i] : maxNum;
+    }
+    answer = maxNum;
+  }
+
+  return answer;
+}
+
+let arr = [128, 460, 603, 40, 521, 137, 123];
+console.log(solution(7, arr));
+
+// 강사님 답
+function solution(n, arr) {
+  let answer;
+  let max = Number.MIN_SAFE_INTEGER;
+  for (let x of arr) {
+    let sum = 0,
+      tmp = x;
+    while (tmp) {
+      sum += tmp % 10;
+      tmp = Math.floor(tmp / 10);
+    }
+    if (sum > max) {
+      max = sum;
+      answer = x;
+    } else if (sum === max) {
+      if (x > answer) answer = x;
+    }
+  }
+  return answer;
+}
+
+let arr = [128, 460, 603, 40, 521, 137, 123];
+console.log(solution(7, arr));
