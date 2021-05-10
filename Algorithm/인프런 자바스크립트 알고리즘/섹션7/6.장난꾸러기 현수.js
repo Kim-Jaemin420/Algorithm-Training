97ccab1ec1d2451931ddaa1420c9b605d83cfeb5
Kim-Jaemin420 <jany240@naver.com>
2021-05-10 10:24:47 +0900

    장난꾸러기 현수

---
+++ Algorithm/인프런 자바스크립트 알고리즘/섹션7/6.장난꾸러기 현수.js
@@ -0,0 +1,26 @@
+// 내 답
+function solution(arr) {
+  let answer = [];
+  const arrSort = [...arr];
+  arrSort.sort();
+
+  for (let i = 0; i < arrSort.length; i++) {
+    if (arrSort[i] !== arr[i]) answer.push(i + 1);
+  }
+
+  return answer;
+}
+
+let arr = [120, 125, 152, 130, 135, 135, 143, 127, 160];
+console.log(solution(arr));
+
+// 강사님 답
+function solution(arr) {
+  let answer = [];
+  let sortArr = arr.slice();
+  sortArr.sort((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== sortArr[i]) answer.push(i + 1);
+  }
+  return answer;
+}
