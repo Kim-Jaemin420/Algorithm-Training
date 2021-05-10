225c4cf29c7b311ac79fe8af9585e5e70de09da9
Kim-Jaemin420 <jany240@naver.com>
2021-05-10 10:42:31 +0900

    좌표정렬

---
+++ Algorithm/인프런 자바스크립트 알고리즘/섹션7/7.좌표정렬.js
@@ -0,0 +1,40 @@
+// 내 답
+function solution(arr) {
+  let answer = arr;
+  let tmp;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i][0] > arr[j][0]) {
+        tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+      } else if (arr[i][0] === arr[j][0] && arr[i][1] > arr[j][1]) {
+        tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+      }
+    }
+  }
+
+  return answer;
+}
+
+let arr = [
+  [2, 7],
+  [1, 3],
+  [1, 2],
+  [2, 5],
+  [3, 6],
+];
+console.log(solution(arr));
+
+// 강사님 답
+function solution(arr) {
+  let answer = arr;
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) return a[1] - b[1];
+    else return a[0] - b[0];
+  });
+  return answer;
+}
