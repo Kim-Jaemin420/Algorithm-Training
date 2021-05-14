dfc550306cc3a9448888fa43b3f6a2ea3a865123
Kim-Jaemin420 <jany240@naver.com>
2021-05-14 13:39:01 +0900

    결혼식 2회차

--- Algorithm/인프런 자바스크립트 알고리즘/섹션7/9.결혼식.js
+++ Algorithm/인프런 자바스크립트 알고리즘/섹션7/9.결혼식.js
@@ -44,3 +44,31 @@ function solution(times) {
   }
   return answer;
 }
+
+/////////////////////////////////////////////
+// 2회차 풀이
+////////////////////////////////////////////
+function solution(times) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  const T_line = [];
+
+  for (let x of times) {
+    T_line.push([x[0], 's']);
+    T_line.push([x[1], 'e']);
+  }
+
+  T_line.sort((a, b) => {
+    if (a[0] === b[0]) return a[1].charCodeAt() - b[1].charCodeAt();
+    else return a[0] - b[0];
+  });
+
+  let cnt = 0;
+  for (let x of T_line) {
+    if (x[1] === 's') ++cnt;
+    else --cnt;
+
+    answer = Math.max(cnt, answer);
+  }
+
+  return answer;
+}
