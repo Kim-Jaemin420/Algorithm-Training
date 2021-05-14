66c81ec688830ff8b8dfc2efc666bfe800b97f82
Kim-Jaemin420 <jany240@naver.com>
2021-05-14 12:53:03 +0900

    좌표정렬 2회차

--- Algorithm/인프런 자바스크립트 알고리즘/섹션7/7.좌표정렬.js
+++ Algorithm/인프런 자바스크립트 알고리즘/섹션7/7.좌표정렬.js
@@ -38,3 +38,17 @@ function solution(arr) {
   });
   return answer;
 }
+
+/////////////////////////////////////////////
+// 2회차 풀이
+////////////////////////////////////////////
+function solution(arr) {
+  let answer = arr;
+
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) return a[1] - b[1];
+    else return a[0] - b[0];
+  });
+
+  return answer;
+}
