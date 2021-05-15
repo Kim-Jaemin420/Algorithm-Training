ce9f5935d5ac1f687a45ca7cacd5ed01a975f75e
Kim-Jaemin420 <jany240@naver.com>
2021-05-15 22:59:25 +0900

    이분검색 2회차

--- Algorithm/인프런 자바스크립트 알고리즘/섹션7/10.이분검색.js
+++ Algorithm/인프런 자바스크립트 알고리즘/섹션7/10.이분검색.js
@@ -1,41 +1,61 @@
-// 내 답
+// // 내 답
+// function solution(target, arr) {
+//   let answer;
+//   let start = 0;
+//   let end = arr.length - 1;
+
+//   arr.sort((a, b) => a - b);
+
+//   while (arr[answer] !== target) {
+//     answer = parseInt((start + end) / 2, 10);
+
+//     if (arr[answer] > target) {
+//       end = answer - 1;
+//     } else if (arr[answer] < target) {
+//       start = answer + 1;
+//     }
+//   }
+//   ++answer;
+//   return answer;
+// }
+
+// let arr = [23, 87, 65, 12, 57, 32, 99, 81];
+// console.log(solution(32, arr));
+
+// // 강사님 답
+// function solution(target, arr) {
+//   let answer;
+//   arr.sort((a, b) => a - b);
+//   let lt = 0;
+//   let rt = arr.length - 1;
+//   while (lt <= rt) {
+//     let mid = parseInt((lt + rt) / 2);
+//     if (arr[mid] === target) {
+//       answer = mid + 1;
+//       break;
+//     } else if (arr[mid] > target) rt = mid - 1;
+//     else lt = mid + 1;
+//   }
+
+//   return answer;
+// }
+
+/////////////////////////////////////////////
+// 2회차 풀이
+////////////////////////////////////////////
 function solution(target, arr) {
-  let answer;
-  let start = 0;
-  let end = arr.length - 1;
-
-  arr.sort((a, b) => a - b);
-
-  while (arr[answer] !== target) {
-    answer = parseInt((start + end) / 2, 10);
-
-    if (arr[answer] > target) {
-      end = answer - 1;
-    } else if (arr[answer] < target) {
-      start = answer + 1;
-    }
-  }
-  ++answer;
-  return answer;
-}
-
-let arr = [23, 87, 65, 12, 57, 32, 99, 81];
-console.log(solution(32, arr));
-
-// 강사님 답
-function solution(target, arr) {
-  let answer;
-  arr.sort((a, b) => a - b);
+  let answer = 0;
   let lt = 0;
   let rt = arr.length - 1;
-  while (lt <= rt) {
-    let mid = parseInt((lt + rt) / 2);
-    if (arr[mid] === target) {
-      answer = mid + 1;
-      break;
-    } else if (arr[mid] > target) rt = mid - 1;
-    else lt = mid + 1;
-  }
+  arr.sort((a, b) => a - b);
 
+  while (answer !== target) {
+    let mid = parseInt((lt + rt) / 2, 10);
+    if (arr[mid] === mid) answer = target;
+    else if (arr[mid] > target) rt = mid - 1;
+    else if (arr[mid] < target) lt = mid + 1;
+  }
   return answer;
 }
+let arr = [23, 87, 65, 12, 57, 32, 99, 81];
+console.log(solution(32, arr));
