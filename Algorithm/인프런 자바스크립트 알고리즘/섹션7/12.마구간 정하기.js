a7f1018ae8cedc509d0140ac100821d84ab19e55
Kim-Jaemin420 <jany240@naver.com>
2021-05-20 11:38:59 +0900

    마구간 정하기 2회차

--- Algorithm/인프런 자바스크립트 알고리즘/섹션7/12.마구간 정하기.js
+++ Algorithm/인프런 자바스크립트 알고리즘/섹션7/12.마구간 정하기.js
@@ -34,3 +34,44 @@ function solution(c, stable) {
   }
   return answer;
 }
+
+/////////////////////////////////////////////
+// 2회차 풀이
+////////////////////////////////////////////
+function count(stable, dist) {
+  let cnt = 1;
+  // 바로 전 말이 들어가있는 자리
+  let endpoint = stable[0];
+
+  for (let i = 0; i < stable.length; i++) {
+    if (stable[i] - endpoint >= dist) {
+      ++cnt;
+      endpoint = stable[i];
+    }
+  }
+
+  return cnt;
+}
+
+function solution(c, stable) {
+  let answer;
+  stable.sort((a, b) => a - b);
+
+  // lt = 두 말 사이의 최소 거리(거리니까 무조건 1)
+  let lt = 1;
+  // rt = 두 말 사이의 최대 거리
+  let rt = stable[stable.length - 1];
+
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+    if (count(stable, mid) >= c) {
+      answer = mid;
+      lt = mid + 1;
+    } else rt = mid - 1;
+  }
+
+  return answer;
+}
+
+let arr = [1, 2, 8, 4, 9];
+console.log(solution(3, arr));
