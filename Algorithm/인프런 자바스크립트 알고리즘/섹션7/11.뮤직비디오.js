827b45289959cb125312627693cc8a24a03e7bcb
Kim-Jaemin420 <jany240@naver.com>
2021-05-20 10:59:05 +0900

    뮤직비디오 2회차

--- Algorithm/인프런 자바스크립트 알고리즘/섹션7/11.뮤직비디오.js
+++ Algorithm/인프런 자바스크립트 알고리즘/섹션7/11.뮤직비디오.js
@@ -1,58 +1,74 @@
-// 내 답 = 못 품
-function solution(m, songs) {
-  let answer;
+// // 내 답 = 못 품
+// function solution(m, songs) {
+//   let answer;
 
-  return answer;
-}
+//   return answer;
+// }
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(solution(3, arr));
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(solution(3, arr));
 
-// 강사님 답
-function count(songs, capacity) {
-  let cnt = 1,
-    sum = 0;
-  for (let x of songs) {
-    if (sum + x > capacity) {
-      cnt++;
-      sum = x;
-    } else sum += x;
-  }
-  return cnt;
-}
+// // 강사님 답
+// function count(songs, capacity) {
+//   let cnt = 1,
+//     sum = 0;
+//   for (let x of songs) {
+//     if (sum + x > capacity) {
+//       cnt++;
+//       sum = x;
+//     } else sum += x;
+//   }
+//   return cnt;
+// }
 
-function solution(m, songs) {
-  let answer;
-  let lt = Math.max(...songs);
-  let rt = songs.reduce((a, b) => a + b, 0);
-  while (lt <= rt) {
-    let mid = parseInt((lt + rt) / 2);
-    if (count(songs, mid) <= m) {
-      answer = mid;
-      rt = mid - 1;
-    } else {
-      lt = mid + 1;
-    }
-  }
-  return answer;
-}
+// function solution(m, songs) {
+//   let answer;
+//   let lt = Math.max(...songs);
+//   let rt = songs.reduce((a, b) => a + b, 0);
+//   while (lt <= rt) {
+//     let mid = parseInt((lt + rt) / 2);
+//     if (count(songs, mid) <= m) {
+//       answer = mid;
+//       rt = mid - 1;
+//     } else {
+//       lt = mid + 1;
+//     }
+//   }
+//   return answer;
+// }
 
 /////////////////////////////////////////////
 // 2회차 풀이
 ////////////////////////////////////////////
+function count(songs, capacity) {
+  let cnt = 1;
+  let sum = 0;
+
+  for (let i = 0; i < songs.length; i++) {
+    if (sum + songs[i] > capacity) {
+      ++cnt;
+      sum = songs[i];
+    } else sum += songs[i];
+  }
+
+  return cnt;
+}
+
 function solution(m, songs) {
-  let answer;
+  let answer = 0;
+
   let lt = Math.max(...songs);
   let rt = songs.reduce((a, b) => a + b, 0);
+
   while (lt <= rt) {
     let mid = parseInt((lt + rt) / 2);
+
     if (count(songs, mid) <= m) {
       answer = mid;
       rt = mid - 1;
-    } else {
-      lt = mid + 1;
-    }
+    } else lt = mid + 1;
   }
+
   return answer;
 }
 
