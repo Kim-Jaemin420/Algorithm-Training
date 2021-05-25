3123481e5fc51e7822a296eafc260305f9d5835e
Kim-Jaemin420 <jany240@naver.com>
2021-05-25 19:46:45 +0900

    2,버블정렬 3회차

--- Algorithm/인프런 자바스크립트 알고리즘/섹션7/2.버블정렬.js
+++ Algorithm/인프런 자바스크립트 알고리즘/섹션7/2.버블정렬.js
@@ -1,49 +1,49 @@
-// 내 답
-function solution(arr) {
-  let answer = arr;
+// // 내 답
+// function solution(arr) {
+//   let answer = arr;
 
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
-      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-    }
-  }
-  return answer;
-}
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - 1; j++) {
+//       if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//     }
+//   }
+//   return answer;
+// }
 
-let arr = [13, 5, 11, 7, 23, 15];
-console.log(solution(arr));
+// let arr = [13, 5, 11, 7, 23, 15];
+// console.log(solution(arr));
 
-// 강사님 답
-function solution(arr) {
-  let answer = arr;
-  for (let i = 0; i < arr.length - 1; i++) {
-    // i 가 0일때, 그러니까 첫 번째 정렬때 가장 큰 수가
-    // 제일 뒤에 놓이게 된다.
-    // 두 번째 큰 수는 두 번째 정렬 때 결정
-    // 따라서 j가 배열 끝까지 돌 필요가 없다.
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      }
-    }
-  }
-  return answer;
-}
+// // 강사님 답
+// function solution(arr) {
+//   let answer = arr;
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     // i 가 0일때, 그러니까 첫 번째 정렬때 가장 큰 수가
+//     // 제일 뒤에 놓이게 된다.
+//     // 두 번째 큰 수는 두 번째 정렬 때 결정
+//     // 따라서 j가 배열 끝까지 돌 필요가 없다.
+//     for (let j = 0; j < arr.length - i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//   }
+//   return answer;
+// }
 
-/////////////////////////////////////////////
-// 2회차 풀이
-////////////////////////////////////////////
-function solution(arr) {
-  let answer = arr;
+// /////////////////////////////////////////////
+// // 2회차 풀이
+// ////////////////////////////////////////////
+// function solution(arr) {
+//   let answer = arr;
 
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
-      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-    }
-  }
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length - 1; j++) {
+//       if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//     }
+//   }
 
-  return answer;
-}
+//   return answer;
+// }
 
 /////////////////////////////////////////////
 // 3회차 풀이
@@ -52,11 +52,14 @@ function solution(arr) {
   let answer = arr;
 
   for (let i = 0; i < arr.length - 1; i++) {
+    let swap = false;
     for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swap = true;
       }
     }
+    if (!swap) return arr;
   }
   return answer;
 }
