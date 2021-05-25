6f5c468eb7da3da47ba319731ef076357951ec5d
Kim-Jaemin420 <jany240@naver.com>
2021-05-25 19:43:36 +0900

    2.버블정렬 3회차

--- Algorithm/인프런 자바스크립트 알고리즘/섹션7/2.버블정렬.js
+++ Algorithm/인프런 자바스크립트 알고리즘/섹션7/2.버블정렬.js
@@ -44,3 +44,22 @@ function solution(arr) {
 
   return answer;
 }
+
+/////////////////////////////////////////////
+// 3회차 풀이
+////////////////////////////////////////////
+function solution(arr) {
+  let answer = arr;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return answer;
+}
+
+let arr = [13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
