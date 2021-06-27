159f69ec1ab04ab5a1bd4a18006f9bd47597f30a
Kim-Jaemin420 <jany240@naver.com>
2021-06-27 19:54:12 +0900

    소수 구하기

---
+++ 백준알고리즘/9단계-기본수학2/소수 구하기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+M, N = map(int, input().split())
+
+a = [False, False] + [True]*(N - 1)
+primes = []
+
+for i in range(2, N + 1):
+    if a[i]:
+        primes.append(i)
+        for j in range(2 * i, N + 1, i):
+            a[j] = False
+
+for i in primes:
+    if M <= i <= N:
+        print(i)
