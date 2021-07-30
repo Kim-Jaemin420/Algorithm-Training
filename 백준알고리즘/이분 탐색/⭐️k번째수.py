72fdfb56423dcad37b6082a38a40c28e2674a237
Kim-Jaemin420 <jany240@naver.com>
2021-07-30 13:34:28 +0900

    k번째 수

--- 백준알고리즘/이분 탐색/⭐️k번째수.py
+++ 백준알고리즘/이분 탐색/⭐️k번째수.py
@@ -1,11 +1,37 @@
+# N, K = int(input()), int(input())
+# sp, ep = 1, K
+
+# while sp <= ep:
+#     md = (sp + ep) // 2
+
+#     cnt = 0
+#     for i in range(1, N+1):
+#         cnt += min(md // i, N)
+
+#     if cnt >= K:
+#         answer = md
+#         ep = md - 1
+#     else:
+#         sp = md + 1
+
+# print(answer)
+
+####################################
+# 2회차 풀이
+####################################
+import sys
+input = sys.stdin.readline
+
 N, K = int(input()), int(input())
-sp, ep = 1, K
+
+sp = 1
+ep = K
 
 while sp <= ep:
     md = (sp + ep) // 2
 
     cnt = 0
-    for i in range(1, N+1):
+    for i in range(1, N + 1):
         cnt += min(md // i, N)
 
     if cnt >= K:
@@ -13,5 +39,4 @@ while sp <= ep:
         ep = md - 1
     else:
         sp = md + 1
-
 print(answer)
